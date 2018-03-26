bedfd3c30e861d3e20dcef77edd4e848171221bf
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-03-26 23:10:46 +0530

    testA

--- cognitoTriggerTest/lambda.js
+++ cognitoTriggerTest/lambda.js
@@ -1,6 +1,23 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+exports.handler = function (event, context, callback) {
+	cognito_idp.adminCreateUser({
+		UserPoolId: process.env.UserPoolId_cognitoTestABC, /* required */
+		Username: "amila1991", /* required */
+		DesiredDeliveryMediums: ["SMS"],//[SMS | EMAIL,/* more items */],
+		ForceAliasCreation: false,
+		MessageAction: "RESEND",
+		TemporaryPassword: "Abcs#123",
+		UserAttributes: [],
+		ValidationData: []
+	}, function (error, data) {
+		if (error) {
+			// implement error handling logic here
+			throw error;
+		}
+		// your logic goes within this block
+	});
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
